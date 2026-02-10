970bbab8c0c9d3fdf5a23da51edaa2f92b65bcac
olusprogr <olus.main333@gmail.com>
2026-02-10 11:01:08 +0100

    awdad

--- test/main.py
+++ test/main.py
@@ -3,7 +3,7 @@ import datasetloader as dl
 import neuralnetwork as nn
 import activation as act
 
-train = False
+train = True
 
 if tf.config.list_physical_devices('GPU'):
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
